663f3f7e710f7c71d8cf1c25c4f21e37f5c11ec6
Avt0r <ruslanmotriuk@gmail.com>
2023-04-27 13:55:51 +0300

    Система ввода и класс прородитель mob

---
+++ Assets/Scripts/Input/InputHandler.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace InputSytem
+{
+    public abstract class InputHandler : MonoBehaviour
+    {
+
+    }
+}
---
+++ Assets/Scripts/Input/InputHandlerPC.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace InputSytem
+{
+    public class InputHandlerPC : MonoBehaviour
+    {
+        [SerializeField] private GameInput _gameInput;
+    }
+}
